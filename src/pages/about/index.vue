42e7b23905aaec0eac317a781135128a896ed3d2
Jardel Bordignon <jardel1101@gmail.com>
2024-08-23 11:01:54 -0300

    :sparkles: config vuetify

---
+++ src/pages/about/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <v-icon icon="mdi-information" />
+    <h1>About Page</h1>
+
+    <NuxtLink to="/">Home Page</NuxtLink>
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <v-icon icon="mdi-home" />
+    <h1>Home Page</h1>
+
+    <NuxtLink to="/about">About Page</NuxtLink>
+
+    <v-calendar
+      style="width: 100%; max-width: 500px; margin: 0 auto"
+      lang="pt-br"
+    />
+  </div>
+</template>
